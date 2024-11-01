72afb73bd3c237943aadac2ed3e3acae6b72c36a
Jordy-Segura <marcelo2005jmsp@gmail.com>
2024-11-01 05:59:29 -0500

    prueba 1}

---
+++ src/main/java/ec/edu/espoch/prueba443/Prueba443.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.prueba443;
+
+/**
+ *
+ * @author jordy
+ */
+public class Prueba443 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
